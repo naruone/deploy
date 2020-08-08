4918b5befea81d9de24478724c1457124d23157e
corey <18507132766@163.com>
2020-08-08 23:38:50 +0800

    优化

--- web/src/views/task/TaskList.vue
+++ web/src/views/task/TaskList.vue
@@ -130,12 +130,6 @@
             </el-pagination>
         </div>
         <TaskEdit :d-type="dType" ref="taskEditorFormDrawer"></TaskEdit>
-
-        <el-steps :active="1" finish-status="success" process-status="process">
-            <el-step title="步骤 1"></el-step>
-            <el-step title="步骤 2"></el-step>
-            <el-step title="步骤 3"></el-step>
-        </el-steps>
     </div>
 </template>
 
